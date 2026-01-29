f3be62c410a305e562a6c2c2f9d0f775f638000e
Anshuman-Awasthi-pro <anshuman.awasthi.work@gmail.com>
2026-01-29 12:32:07 -0500

    Task 5: Get book reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -64,9 +64,16 @@ public_users.get('/title/:title', function (req, res) {
 
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+  
+    if (books[isbn]) {
+      res.send(books[isbn].reviews);
+    } else {
+      res.send("No book found with the given ISBN");
+    }
+  });
+  
 
 module.exports.general = public_users;
