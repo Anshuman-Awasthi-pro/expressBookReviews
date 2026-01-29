7f0c60dbc7fdc9ec1af55740172561e460da8bd3
Anshuman-Awasthi-pro <anshuman.awasthi.work@gmail.com>
2026-01-29 12:39:16 -0500

    Task 6: User registration implemented

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,31 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username or password is missing
+    if (!username || !password) {
+      return res.status(400).json({
+        message: "Username and password are required"
+      });
+    }
+  
+    // Check if user already exists
+    if (users.some(user => user.username === username)) {
+      return res.status(409).json({
+        message: "User already exists"
+      });
+    }
+  
+    // Register new user
+    users.push({ username, password });
+  
+    return res.status(200).json({
+      message: "User successfully registered"
+    });
+  });
+  
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -16,7 +37,6 @@ public_users.get('/',function (req, res) {
     return res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
@@ -29,7 +49,6 @@ public_users.get('/isbn/:isbn', function (req, res) {
   });
   
   
-// Get book details based on author
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
