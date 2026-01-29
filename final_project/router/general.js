7ce570fc9337ed9447cce3b988e8da920f79ed13
Anshuman-Awasthi-pro <anshuman.awasthi.work@gmail.com>
2026-01-29 12:19:18 -0500

    Task 3: Get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,10 +30,22 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let result = {};
+  
+    // Get all ISBN keys from books object
+    Object.keys(books).forEach((key) => {
+      if (books[key].author === author) {
+        result[key] = books[key];
+      }
+    });
+  
+    // Send matching books
+    res.send(JSON.stringify(result, null, 4));
+  });
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
